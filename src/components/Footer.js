321022757a7993bbc90de78a51879955e6312cff
K Manohar <114045942+Manohar-code3@users.noreply.github.com>
2025-08-14 00:54:47 +0530

    some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="udyam-footer">
       <div>
-        UDYAM REGISTRATION &copy; {new Date().getFullYear()} | Ministry of MSME | Project by <b>Manohar/k3manohar@gmail.com</b>
+        UDYAM REGISTRATION &copy; {new Date().getFullYear()} | Ministry of MSME | Details <b>Manohar/k3manohar@gmail.com</b>
       </div>
       <div style={{marginTop:6, fontSize:"13px"}}>
         For help: champions@msme.gov.in
